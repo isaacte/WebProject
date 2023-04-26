cce4276c25d59fa1db04c6f6ed162155a8257108
idroj2002 <jordiarenasr@gmail.com>
2023-04-26 12:24:59 +0200

    change directorys

--- Base/views.py
+++ Base/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "Base/index.html")
+    return render(request, "../templates/Base/index.html")
