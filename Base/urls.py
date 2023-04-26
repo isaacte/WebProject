ced88d26d3f42883d59fadf7c1e954807ae6095e
WhiteLion03 <sergi.baron.pascual@gmail.com>
2023-04-26 12:51:04 +0200

    Implemented register and html (index, register)

--- Base/urls.py
+++ Base/urls.py
@@ -2,11 +2,11 @@
 
 from django.urls import include, path
 from django.contrib.auth import views
-from Base.views import menu
+from .views import menu, RegisterView
 
 urlpatterns = [
     path('', menu, name='home'),
-    #path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- Base/views.py
+++ Base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import models
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    model = models.User
+    fields = ['username', 'password1', 'password2']
 
 def menu(request):
     return render(request, 'Base/base.html')
