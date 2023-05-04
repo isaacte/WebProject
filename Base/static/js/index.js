3b2690a2c227f7a1c3928beb0b5b2e349500c7e2
idroj2002 <jordiarenasr@gmail.com>
2023-05-04 23:18:38 +0200

    API with REST framework

--- Base/static/js/index.js
+++ Base/static/js/index.js
@@ -1,23 +1,29 @@
 // Return a list of books from a json object. Return null if there aren't any book.
 const listBooks = async() => {
     try {
-        const response = await fetch("./books");
+        const response = await fetch("./api/books");
         const data = await response.json();
-        if (data["message"] == "success") {
-            return data["books"];
-        } else {
+        console.log(data[0]);
+        if (data.length == 0) {
             return null;
+        } else {
+            return data;
         }
     } catch (error) {
         console.log(error);
     }
 }
 
+// Return the authors of a book
+const getAuthor = (book) => {
+    
+}
+
 // Return the html of a book card from a book object
 const getBookCard = (book) => {
     return `<div class="col-3 col-sm-6 col-md-4 col-lg-3">
         <div class="card mb-3">
-           <img src="../../static/images/loading_background.avif" class="card-img-top" alt="INSERTAR AQUÍ IMAGEN">
+           <img src="${book["image"]}" class="card-img-top" alt="INSERTAR AQUÍ IMAGEN">
            <div class="card-body">
                 <h5 class="card-title">${book["title"]}</h5>
                 <p class="card-text">${book["summary"]}</p>
