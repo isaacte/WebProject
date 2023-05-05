f94a7ee230ccaacb137848db9a53174b994806e4
Isaac Torres Espuña <isaac@ite2002.net>
2023-05-05 10:59:01 +0200

    Added authors to book serializers.py

--- Base/serializers.py
+++ Base/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from .models import Book, Author, AuthorInBook
 
+
+def get_authors(obj):
+    authors = []
+    for author_in_book in AuthorInBook.objects.filter(book=obj):
+        serializer = AuthorSerializer(author_in_book.author)
+        authors.append(serializer.data)
+    return authors
+
+
 class BookSerializer(serializers.ModelSerializer):
+    authors = serializers.SerializerMethodField()
     class Meta:
         model = Book
-        fields = ('ISBN', 'title', 'publish_date', 'pages_number', 'summary', 'edition', 'image', 'language')
+        fields = ('ISBN', 'title', 'publish_date', 'pages_number', 'summary', 'edition', 'image', 'language', 'authors')
+
 
 class AuthorInBookSerializer(serializers.ModelSerializer):
     class Meta:
