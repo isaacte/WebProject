beb8003a35d2a084f520516fe5deda84b35792c3
WhiteLion03 <sergi.baron.pascual@gmail.com>
2023-05-19 19:16:30 +0200

    adding reviews implemented

--- Base/admin.py
+++ Base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, LiteraryGenre, Book, LiteraryGenreInBook, AuthorInBook, Review
+from .models import Author, LiteraryGenre, Book, LiteraryGenreInBook, AuthorInBook, Review, BookInUserLibrary
 
 # Register your models here.
 admin.site.register(Author)
@@ -8,3 +8,4 @@ admin.site.register(Book)
 admin.site.register(LiteraryGenreInBook)
 admin.site.register(AuthorInBook)
 admin.site.register(Review)
+admin.site.register(BookInUserLibrary)
--- Base/views.py
+++ Base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
-from Base.forms import RegisterForm
+from .forms import RegisterForm, ReviewForm
 from django.http import HttpRequest, HttpResponseBadRequest, HttpResponseNotFound
 from .models import Author, BookInUserLibrary, Review
 from django.shortcuts import get_object_or_404
@@ -22,6 +23,7 @@ def register(request : HttpRequest):
 def index(request):
     return render(request, 'Base/index.html')
 
+@login_required
 def my_books(request):
     return render(request, 'Base/my_books.html')
 
@@ -29,14 +31,29 @@ def author(request, author_id):
     a = get_object_or_404(Author, openlibrary_key = author_id)
     return render(request, 'Base/author.html', {'author': a})
 
-def book(request, key):
-    b = get_book(key)
+def book(request, book_id):
+    b = get_book(book_id)
     if not b:
         return HttpResponseNotFound("Book not found.")
     a = b.authorinbook_set.all()
     g = b.literarygenreinbook_set.all()
     r = Review.objects.filter(book = b)
-    return render(request, 'Base/book.html', {'book': b, 'authors': a, 'genres': g, 'reviews': r})
+    form = ReviewForm()
+    return render(request, 'Base/book.html', {'book': b, 'authors': a, 'genres': g, 'reviews': r, 'form': form})
+
+@login_required
+def create_review(request, book_id):
+    if request.method != 'POST':
+        return HttpResponseBadRequest(f"Method not allowed.", status=405)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.user = request.user
+        review.book = get_book(book_id)
+        review.save()
+        return redirect('book', book_id=book_id)
+    form = ReviewForm()
+    return redirect('book', book_id=book_id)
 
 def search_book(request, query):
     result = requests.get(f'https://openlibrary.org/search.json?q={query}')
